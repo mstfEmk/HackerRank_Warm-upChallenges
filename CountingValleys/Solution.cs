ae97c92c7cd7b310c67144783df2ef9d541952bc
mstfEmk <mstfEmk@gmail.com>
2018-12-05 21:10:11 +0300

    CountingValleys

---
+++ CountingValleys/Solution.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountingValleys
+{
+    class Solution
+    {
+        // Complete the countingValleys function below.
+        static int countingValleys(int n, string s)
+        {
+            int level = 0;
+            bool inValley = false;
+            int valleyCount = 0;
+
+            foreach (char c in s)
+            {
+                if (c == 'U')
+                {
+                    level++;
+                } else if (c == 'D')
+                {
+                    level--;
+                }
+
+                if (level < 0)
+                {
+                    inValley = true;
+                }
+
+                if (inValley == true)
+                {
+                    if (level == 0)
+                    {
+                        valleyCount++;
+                        inValley = false;
+                    }
+                }
+
+            }
+            return valleyCount;
+        }
+
+        static void Main(string[] args)
+        {
+            // 8 UDDDUDUU => 1
+            // 12 DDUUDDUDUUUD => 2
+
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            string s = Console.ReadLine();
+
+            int result = countingValleys(n, s);
+
+            Console.WriteLine(result);
+
+        }
+    }
+}
